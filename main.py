31b29888962f056a339467e5c7a6f57360ae9ac8
acanus <sdlgentleboys@gmail.com>
2024-10-22 23:11:03 +0700

    Fix camera_thread_func to capture image data correctly

--- main.py
+++ main.py
@@ -307,7 +307,7 @@ def camera_thread_func():
                     for frame in cam.get_frame_generator(limit=1, timeout_ms=5000):
                         print('Got {}'.format(frame), flush=True)
                         bgr_frame = frame.convert_pixel_format(PixelFormat.Bgr8)
-                        image_captured_data = bgr_frame.as_opencv_image()
+                        image_captured_data = bgr_frame.as_opencv_image().copy()
                     image_captured_event.set()
                     capture_event.clear()
                     print('Image captured', flush=True)
